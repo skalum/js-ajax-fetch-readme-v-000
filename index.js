2cd988dc87d8491cf357a2b30b425151572de719
skalum <stkalum@gmail.com>
2018-06-22 17:22:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const app = "I don't do much.";
+
+const token = '9f89a7dd3fd5d8c7f0487cebbedb15a7ba7be8bd';
+
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
